fa46911b93d77e9a76988c3e583a9671a4bdfca4
Johannes Weissmann <jo@starsheriff.eu>
2018-07-05 16:40:45 +0200

    returning specific error on conflict

--- errors.go
+++ errors.go
@@ -9,4 +9,6 @@ var (
 
 	ErrPreconditionFailed = errors.New("precondition failed")
 	ErrorNotImplemented   = errors.New("not implemented")
+
+	ErrConflict = errors.New("Resource with specified id or name already exists.")
 )
