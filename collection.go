8d12e706822ff6a86acc60a31f9ee4f9f0d5d61e
Torstein Nicolaysen <torstein.nicolaysen@vipps.no>
2018-08-19 14:40:30 +0200

    fix typo in Collection struct. sporcs -> sprocs

--- collection.go
+++ collection.go
@@ -9,7 +9,7 @@ type Collection struct {
 	IndexingPolicy *IndexingPolicy `json:"indexingPolicy,omitempty"`
 	Docs           string          `json:"_docs,omitempty"`
 	Udf            string          `json:"_udfs,omitempty"`
-	Sporcs         string          `json:"_sporcs,omitempty"`
+	Sprocs         string          `json:"_sprocs,omitempty"`
 	Triggers       string          `json:"_triggers,omitempty"`
 	Conflicts      string          `json:"_conflicts,omitempty"`
 	PartitionKey   *PartitionKey   `json:"partitionKey,omitempty"`
