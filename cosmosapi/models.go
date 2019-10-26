fe2d845d27dc7cef97be4a8402cc274487f4008d
Espen Stene Brønstad <espen.bronstad@vipps.no>
2019-10-26 10:28:57 +0200

    Add order to composite index

--- cosmosapi/models.go
+++ cosmosapi/models.go
@@ -18,6 +18,13 @@ const (
 	Spatial = IndexKind("Spatial")
 )
 
+type IndexOrder string
+
+const (
+	Ascending  IndexOrder = "ascending"
+	Descending IndexOrder = "descending"
+)
+
 const MaxPrecision = -1
 
 type Index struct {
@@ -36,7 +43,8 @@ type ExcludedPath struct {
 }
 
 type CompositeIndex []struct {
-	Path string `json:"path"`
+	Path  string     `json:"path"`
+	Order IndexOrder `json:"order,omitempty"`
 }
 
 // Stored Procedure
